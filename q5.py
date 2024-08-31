36fdba98d29a0a31b63b93ade303c636ef50ea1e
Harshit Jangir <it.s.me.of.em@gmail.com>
2024-08-31 12:58:20 +0530

    Answer 5

--- q5.py
+++ q5.py
@@ -1,7 +1,13 @@
+# the language of the question is very confusing and doesn't tell enough things to make the program, like 1st customer
+# has viewed all the products which 5th customer has viewed then shouldn't 5th be recommended product 4.
+# and the question also doesn't specify if all the products should match in order to apply that recommendation rule or
+# at least one product should match. and what should be the recommendation order if product view count is same for two
+# products. so I have to make a lot of assumptions and THE OUTPUT WOULDN'T BE SAME. I still tried to make something
+
 from utils import remove_duplicates
 
 items = [
-    [1, 2, 3, 4],
+    [1, 2, 3, 4, 5],
     [1, 2, 4, 5],
     [1, 3, 4, 5],
     [2, 3, 5],
@@ -11,12 +17,8 @@ items = [
 for i in range(len(items)):
     items[i].sort()
 
-# the language of the question is very confusing and doesn't tell enough things to make the program, like 1st customer
-# has viewed all the products which 5th customer has viewed then shouldn't 5th be recommended product 4.
-# and the question also doesn't specify if all the products should match in order to apply that recommendation rule or
-# at least one product should match. so I have to make a lot of assumptions and the output wouldn't be same
-
 # Overall Most Viewed Products
+
 simplifiedItems = []
 for i in range(len(items)):
     for j in range(len(items[i])):
@@ -27,6 +29,7 @@ sortedUniqueSimplifiedItems = []
 remove_duplicates(sortedSimplifiedItems, sortedUniqueSimplifiedItems)
 
 # creating 2d output array
+
 recommendation = []
 for i in range(len(items)):
     recommendation.append(0)
@@ -35,15 +38,24 @@ for i in range(len(items)):
 # making recommendation
 
 for i in range(len(items)):
-    tempItems = items.pop(i)
-    print(tempItems)
-    for j in range(len(items) - 1):
+    for j in range(len(items)):
+        match = True
+        tempJ = items[j].copy()
         for k in range(len(items[i])):
             if items[i][k] not in items[j]:
-                uselessVar = 1
+                match = False
             else:
-                if len(items[i]) < len(items[j]):
-                    for l in range(len(items[j]) - len(items[i])):
-                        recommendation[i].append(items[j][len(items[i])+l-1])
+                tempJ.remove(items[i][k])
+        if match and len(items[i]) < len(items[j]):
+            # for l in range(len(items[j]) - len(items[i])):
+            #     recommendation[i].append(items[j][len(items[i])+l])
+            for l in range(len(tempJ)):
+                recommendation[i].append(tempJ[l])
+
+# adding remaining recommendations
+
+for i in range(len(recommendation)):
+    for j in range(3 - len(recommendation[i])):
+        recommendation[i].append(sortedUniqueSimplifiedItems[j])
 
 print(recommendation)
